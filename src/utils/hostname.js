3638a5ed1843bf8480773dd7fa9ec0d346e11aae
premj01 <premjadhav00002@gmail.com>
2025-10-27 12:33:40 +0530

    functanality lookup

--- src/utils/hostname.js
+++ src/utils/hostname.js
@@ -1,5 +1,5 @@
-// const hostname = "https://make-my-study-plan-backend.onrender.com";
-const hostname = "http://localhost:9000";
+const hostname = "https://make-my-study-plan-backend.onrender.com";
+// const hostname = "http://localhost:9000";
 // const hostname = "https://62wthqft-9000.inc1.devtunnels.ms";
 
 
